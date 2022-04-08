14923aedb4a7dcb7fdf4d80ea2a25167fca9f41d
Richard Kim <Richard.kim.0000@gmail.com>
2022-04-08 02:55:20 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+// EXPRESS MIDDLEWARE
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://richardkim:Cup1dssparrow@cluster0.vbs32.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`SERVER RUNNING ON PORT: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+// Using MongoDB cloud atlas
