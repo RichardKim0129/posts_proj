a78ba28f46fb3f2ddd899b839a8f5b25f8d0a1d6
Richard Kim <Richard.kim.0000@gmail.com>
2022-04-08 16:39:27 -0700

    redux setup

--- server/index.js
+++ server/index.js
@@ -8,12 +8,12 @@ import postRoutes from "./routes/posts.js";
 const app = express();
 
 // EXPRESS MIDDLEWARE
-app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/posts", postRoutes);
 const CONNECTION_URL =
   "mongodb+srv://richardkim:Cup1dssparrow@cluster0.vbs32.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
