c6b10114d74ed5c8da267677ebb6a1aea83da606
Richard Kim <Richard.kim.0000@gmail.com>
2022-04-20 19:19:10 -0700

    Working on form

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">
-          Memories
+          Journalie
         </Typography>
         <img
           className={classes.image}
